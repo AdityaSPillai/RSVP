7e6967b297b41a22c1114abfcaaaea49fdf94843
Aditya S <119068813+AdityaSPillai@users.noreply.github.com>
2025-12-18 18:58:00 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/v1",
+    baseURL: process.env.NODE_ENV === 'production'
+        ? 'https://rsvp-backend-1j8j.onrender.com/v1'
+        : 'http://127.0.0.1:5000/v1',
 });
 
 api.interceptors.request.use(

cd10c9ec15163459104afb988256af053bbffd94
Aditya S <119068813+AdityaSPillai@users.noreply.github.com>
2025-12-18 18:53:15 +0530

    Update axios.js

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 const instance = axios.create({
   baseURL:
     process.env.NODE_ENV === 'production'
-      ? 'https://rsvp-backend-1j8j.onrender.com'
+      ? 'https://rsvp-backend-1j8j.onrender.com/v1'
       : 'http://127.0.0.1:5000/v1',
   timeout: 10000,
   headers: {
